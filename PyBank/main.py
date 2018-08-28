45971fdd7dd07cbed44d1a90f90f5e5b48df3c4a
Christie Lafrance <christie.lafrance@gmail.com>
2018-08-27 23:30:18 -0700

    change output file type, from csv to text

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,9 @@
+# -- The total number of months included in the dataset
+# -- The total net amount of "Profit/Losses" over the entire period
+# -- The average change in "Profit/Losses" between months over the entire period
+# -- The greatest increase in profits (date and amount) over the entire period
+# -- The greatest decrease in losses (date and amount) over the entire period
+# -- print the analysis to the terminal and export a text file with the results.
 
 import os
 import csv
@@ -32,9 +38,12 @@ with open(csvpath, newline='') as csvfile:
 		total_profit_loss_change = total_profit_loss_change + profit_loss_change
 
 		if profit_loss_change > greatest_increase_in_profit_loss:
+
 			greatest_increase_in_profit_loss = profit_loss_change
 			greatest_increase_month = row[0]
+
 		elif profit_loss_change < greatest_decrease_in_profit_loss:
+
 			greatest_decrease_in_profit_loss = profit_loss_change
 			greatest_decrease_month = row[0]
 
@@ -46,29 +55,43 @@ with open(csvpath, newline='') as csvfile:
 	print("Financial Analysis")
 	print("-" * 40)
 	print("")
-	print(f'Total Months: {total_num_months}')
-	print(f'Total: ${round(total_profit_loss)}')
-	print(f'Average  Change: ${round(average_monthly_change, 2)}')
-	print(f'Greatest Increase in Profits: {greatest_increase_month} (${round(greatest_increase_in_profit_loss)})')
-	print(f'Greatest Decrease in Profits: {greatest_decrease_month} (${round(greatest_decrease_in_profit_loss)})')
+	print(f'Total Months: { total_num_months }')
+	print(f'Total: ${ round(total_profit_loss) }')
+	print(f'Average  Change: ${ round(average_monthly_change, 2) }')
+	print(f'Greatest Increase in Profits: { greatest_increase_month } (${ round(greatest_increase_in_profit_loss) })')
+	print(f'Greatest Decrease in Profits: { greatest_decrease_month } (${ round(greatest_decrease_in_profit_loss) })')
 	print("")
 
 	if not os.path.isdir('../PyBank/output'):
 		os.makedirs('../PyBank/output')
 
-	output_path = os.path.join("../PyBank", "output", "bank_data_analysis.csv")
+	outputfile = '../PyBank/output/bank_data_analysis.txt'
 
-	with open(output_path, 'w', newline='') as csvfile:
+	with open(outputfile, 'w') as textfile:
+	
+		textfile.write('Financial Analysis \n')
+		textfile.write("-" * 40)
+		textfile.write("\n")
+		textfile.write(f'Total Months: { total_num_months }\n')
+		textfile.write(f'Total: ${ round(total_profit_loss) }\n')
+		textfile.write(f'Average  Change: ${ round(average_monthly_change, 2) }\n')
+		textfile.write(f'Greatest Increase in Profits: { greatest_increase_month } (${ round(greatest_increase_in_profit_loss) })\n')
+		textfile.write(f'Greatest Decrease in Profits: { greatest_decrease_month } (${ round(greatest_decrease_in_profit_loss) })\n')
 
-		csvwriter = csv.writer(csvfile, delimiter=',')
-		csvwriter.writerow(['Financial Analysis'])
-		csvwriter.writerow(["-" * 40])
-		csvwriter.writerow("")
-		csvwriter.writerow([f'Total Months: {total_num_months}'])
-		csvwriter.writerow([f'Total: ${round(total_profit_loss)}'])
-		csvwriter.writerow([f'Average  Change: ${round(average_monthly_change, 2)}'])
-		csvwriter.writerow([f'Greatest Increase in Profits: {greatest_increase_month} (${round(greatest_increase_in_profit_loss)})'])
-		csvwriter.writerow([f'Greatest Decrease in Profits: {greatest_decrease_month} (${round(greatest_decrease_in_profit_loss)})'])
+
+	# output_path = os.path.join("../PyBank", "output", "bank_data_analysis.csv")
+
+	# with open(output_path, 'w', newline='') as csvfile:
+
+	# 	csvwriter = csv.writer(csvfile, delimiter=',')
+	# 	csvwriter.writerow(['Financial Analysis'])
+	# 	csvwriter.writerow(["-" * 40])
+	# 	csvwriter.writerow("")
+	# 	csvwriter.writerow([f'Total Months: {total_num_months}'])
+	# 	csvwriter.writerow([f'Total: ${round(total_profit_loss)}'])
+	# 	csvwriter.writerow([f'Average  Change: ${round(average_monthly_change, 2)}'])
+	# 	csvwriter.writerow([f'Greatest Increase in Profits: {greatest_increase_month} (${round(greatest_increase_in_profit_loss)})'])
+	# 	csvwriter.writerow([f'Greatest Decrease in Profits: {greatest_decrease_month} (${round(greatest_decrease_in_profit_loss)})'])
 
 		# def as_currency(amount):
 		# 	if amount >= 0:
